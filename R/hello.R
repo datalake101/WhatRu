3efcfbe2addd06e503206b6a8cbd9f2a3f013125
brammaputra <80239178+datalake101@users.noreply.github.com>
2023-02-19 21:19:16 -0500

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -1,37 +1,40 @@
-#whatRu
-# display column types
-
-whatRu <- function(df){
+whatRu <- function(df) {
   suppressWarnings({
-  binary <- character()
-  multinomial <- character()
-  continuous <- character()
-  ordinal <- character()
-  discrete <- character()
+    binary <- character()
+    multinomial <- character()
+    continuous <- character()
+    ordinal <- character()
+    discrete <- character()
+    factor_col <- character()
 
-  for (col in names(df)) {
-    x <- df[[col]]
-    unique_vals <- base::length(unique(x))
-    all_int <- all(x %% 1 == 0)
-    all_nonneg <- all(x >= 0)
-    has_fraction <- any(x %% 1 != 0)
-    if (unique_vals == 2) {
-      binary <- c(binary, col)
-    } else if (all_nonneg && !has_fraction && unique_vals >= 5 && unique_vals < 11) {
-      ordinal <- c(ordinal, col)
-    } else if (!has_fraction && unique_vals >= 5) {
-      discrete <- c(discrete, col)
-    } else if (unique_vals >= 3 && unique_vals < 11) {
-      multinomial <- c(multinomial, col)
-    } else {
-      continuous <- c(continuous, col)
+    for (col in names(df)) {
+      x <- df[[col]]
+      if (is.factor(x)) {
+        factor_col <- c(factor_col, col)
+      } else {
+        unique_vals <- base::length(unique(x))
+        all_int <- all(x %% 1 == 0)
+        all_nonneg <- all(x >= 0)
+        has_fraction <- any(x %% 1 != 0)
+        if (unique_vals == 2) {
+          binary <- c(binary, col)
+        } else if (all_nonneg && !has_fraction && unique_vals >= 5 && unique_vals < 11) {
+          ordinal <- c(ordinal, col)
+        } else if (!has_fraction && unique_vals >= 5) {
+          discrete <- c(discrete, col)
+        } else if (unique_vals >= 3 && unique_vals < 11) {
+          multinomial <- c(multinomial, col)
+        } else {
+          continuous <- c(continuous, col)
+        }
+      }
     }
-  }
 
-  list(binary = binary,
-       multinomial = multinomial,
-       continuous = continuous,
-       ordinal = ordinal,
-       discrete = discrete)
+    list(factor_col = factor_col,
+         binary = binary,
+         multinomial = multinomial,
+         continuous = continuous,
+         ordinal = ordinal,
+         discrete = discrete)
   })
 }
